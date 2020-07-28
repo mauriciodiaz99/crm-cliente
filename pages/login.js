b4dc4d1d21538331dfb59574cf9ec91f892d8032
mauriciodiaz99 <57414223+mauriciodiaz99@users.noreply.github.com>
2020-07-28 13:24:04 +0200

    Login casi terminado

--- pages/login.js
+++ pages/login.js
@@ -6,6 +6,46 @@ const Login = () => {
         <>
         <Layout>
             <h1 className="text-center text-2xl text-white font-light">Login</h1>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-sm">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        <div className="md:flex bg-white rounded-lg justify-center">
+                            <img className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto" src="/img/crm.PNG" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline"
+                                id="email"
+                                type="email"
+                                placeholder="Email Usuario"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline"
+                                id="password"
+                                type="password"
+                                placeholder="Password Usuario"
+                            />
+                        </div>
+
+                        <input 
+                            type="submit"
+                            className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                            value="Iniciar Sesion"
+                        />
+                    </form>
+                </div>
+            </div>
         </Layout>
         </>
      );
