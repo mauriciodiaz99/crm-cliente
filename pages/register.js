55630ffa3466380be6b9c7c1375bf2e3f48fa408
mauriciodiaz99 <57414223+mauriciodiaz99@users.noreply.github.com>
2020-07-28 12:21:50 +0200

    Sidebar Terminado

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Register = () => {
+    return ( 
+        <>
+            <Layout>
+                <h1 className="text-center">Registrate</h1>
+            </Layout>
+        </>
+     );
+}
+ 
+export default Register;
