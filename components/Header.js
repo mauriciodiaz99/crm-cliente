f510a10e17c2362510cedb38e8008c3f2cdb4007
mauriciodiaz99 <57414223+mauriciodiaz99@users.noreply.github.com>
2020-07-29 12:42:37 +0200

    Agregar Clientes Terminado

---
+++ components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+const OBTENER_USUARIO = gql`
+query obtenerUsuario {
+    obtenerUsuario {
+      id
+      nombre
+      apellido
+    }
+  }
+`;
+
+const Header = () => {
+
+    //Routing
+    const router = useRouter();
+
+    //query de apollo
+    const { data, loading, error } = useQuery(OBTENER_USUARIO);
+
+    //Proteger que no accedamos a l adata antes dde tener resultados
+    if(loading) return null;
+
+    //Si no hay informacion
+    if (!data) {
+        return router.push('/login');
+    }
+
+    const { nombre, apellido } = data.obtenerUsuario;
+
+    const cerrarSesion = () => {
+        localStorage.removeItem('token');
+        router.push('/login');
+    }
+
+    return ( 
+        <div className="flex justify-between mb-6">
+            <p className="mr-2">Bienvenido(a): {nombre} {apellido}</p>
+
+            <button 
+                className="bg-blue-800 w-full sm:w-auto font-bold uppercase text-xs rounded py-1 px-2 text-white shadow-md"
+                type="button"
+                onClick={() => cerrarSesion()}
+            >
+                Cerrar Sesión
+            </button>
+        </div>
+     );
+}
+ 
+export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
 import { useRouter } from 'next/router';
 
 const Layout = ({children}) => {
@@ -24,7 +25,9 @@ const Layout = ({children}) => {
                 <div className="bg-gray-200 min-h-screen">
                     <div className="flex min-h-screen">
                         <Sidebar />
+
                         <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                            <Header />
                             {children}
                         </main>
                     </div>
